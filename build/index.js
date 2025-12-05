239ec52e73377d4ef1b3b62b7df02188fc806bf8
Rafael Rodrigo <rafaelrodrigoartes@gmail.com>
2025-12-05 08:16:16 -0300

    ok

---
+++ build/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Rota básica funcionando!');
+});
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 8080');
+});
