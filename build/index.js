f64ddb477494de8e79253c62e4a1ab67f76635ee
Rafael Rodrigo <rafaelrodrigoartes@gmail.com>
2025-12-05 09:18:16 -0300

    sera?

--- build/index.js
+++ build/index.js
@@ -9,10 +9,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, '../public/index.html'));
+    res.sendFile(join(__dirname, "../public/index.html"));
 });
-io.on('connection', (socket) => {
-    console.log('User Conectado');
+io.on("connection", (socket) => {
+    console.log("User Conectado");
 });
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 8080");
