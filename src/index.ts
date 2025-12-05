eaf1cbd3448537cfdf1925f4a82e6888bd5baaa7
Rafael Rodrigo <rafaelrodrigoartes@gmail.com>
2025-12-05 09:02:47 -0300

    Deu certo

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const server = createServer(app)
-const io = new Server(server)
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname,'../public/index.html'))
+  res.sendFile(join(__dirname, "../public/index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('User Conectado')
-})
+io.on("connection", (socket) => {
+  console.log("User Conectado");
+});
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 8080");
